7fd3efffb077048f02138747fd6d330b7860a5e0
j0rn <jorn@aakre.no>
2016-08-26 22:13:43 +0200

    Initial commit

---
+++ COMPort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPortLogger
+{
+    class ComPort : IComparable
+    {
+        public int Port { get; set; }
+        public string Description { get; set; }
+        public DateTime Added { get; set; }
+
+        private static int GetPort(string s)
+        {
+            s = s.Remove(0, s.IndexOf("(COM") + 4);
+            s = s.Substring(0, s.Length - 1);
+            return int.Parse(s);
+        }
+
+        internal static List<ComPort> GetAllPorts()
+        {
+            DateTime added = DateTime.Now;
+            List<ComPort> ret = new List<ComPort>();
+            using (ManagementObjectSearcher searcher =
+                       new ManagementObjectSearcher("root\\CIMV2",
+                       "SELECT * FROM Win32_PnPEntity"))
+            {
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    if (queryObj["Caption"] == null) continue;
+                    string cap = queryObj["Caption"].ToString();
+                    if (cap.Contains("(COM") == false) continue;
+
+                    if (cap.StartsWith("Communications Port")) continue;
+
+                    ComPort c = new ComPort();
+                    c.Added = added;
+                    c.Port = GetPort(cap);
+                    c.Description = cap.Substring(0, cap.IndexOf(" (COM"));
+                    ret.Add(c);
+                }
+            }
+            return ret;
+        }
+
+        public int CompareTo(object obj)
+        {
+            ComPort c2 = obj as ComPort;
+            return this.Added.AddMilliseconds(this.Port).CompareTo(c2.Added.AddMilliseconds(c2.Port));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Management;
+
+namespace COMPortLogger
+{
+    class Program
+    {
+        private static void PrintPorts(List<ComPort> ports, bool first)
+        {
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+            Console.SetCursorPosition(0, 0);
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            ports.ForEach(z =>
+            {
+                Console.Write(z.Added);
+                Console.Write("\t");
+                Console.Write("COM" + z.Port.ToString());
+                Console.Write("\t");
+                Console.Write(z.Description);
+                while (Console.CursorLeft < 70) Console.Write(" "); // To clear rest of line
+                Console.WriteLine();
+            });
+            Console.WriteLine("".PadRight(70));
+
+            if (first == false)
+                Console.SetCursorPosition(x, y);
+        }
+
+        static void Main(string[] args)
+        {
+            List<ComPort> AllPorts = ComPort.GetAllPorts();
+
+            AllPorts.Sort();
+            PrintPorts(AllPorts, true);
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 10);
+
+            while (true)
+            {
+                Console.WindowWidth = 80;
+                Console.WindowHeight = 25;
+                Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
+
+                List<ComPort> ports = ComPort.GetAllPorts();
+
+                // Removed ports
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                AllPorts.ForEach(x =>
+                {
+                    ComPort p = ports.Find(z => z.Port == x.Port);
+                    if (p != null)
+                    {
+                        p.Added = x.Added;
+                    }
+                    else
+                    {
+                        Console.WriteLine(DateTime.Now + "\tCOM" + x.Port + "\t" + x.Description);
+                    }
+                });
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                // Added ports
+                ports.ForEach(x =>
+                {
+                    ComPort p = AllPorts.Find(z => z.Port == x.Port);
+                    if (p == null)
+                    {
+                        Console.WriteLine(DateTime.Now + "\tCOM" + x.Port + "\t" + x.Description);
+                    }
+                });
+
+
+                AllPorts = ports;
+
+                ports.Sort();
+                PrintPorts(ports, false);
+                System.Threading.Thread.Sleep(500);
+            }
+
+        }
+    }
+}
